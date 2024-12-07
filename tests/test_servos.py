92548b668caa25f55049df4f097e965060112409
Ivan Yevenko <iyevenko@gmail.com>
2024-12-06 23:47:13 -0500

    servo testing

--- tests/test_servos.py
+++ tests/test_servos.py
@@ -14,6 +14,9 @@ pwm2 = GPIO.PWM(13, 50)  # Second servo
 legs_out = 6.5     # 90 degrees - legs out
 legs_in = 2.5      # 0 degrees - legs in
 
+# 0.500 ms =  2.5% 20ms/50Hz -> 0 deg
+# 2.500 ms = 12.5% 20ms/50Hz -> 180 deg
+
 def set_servo_positions(duty_cycle):
     pwm1.start(duty_cycle)
     pwm2.start(duty_cycle)
@@ -22,13 +25,16 @@ def set_servo_positions(duty_cycle):
     pwm1.stop()
     pwm2.stop()
 
-# Test servo positions
-print("Moving legs out...")
-set_servo_positions(legs_out)
+# set_servo_positions(12.5) # home
+set_servo_positions(6.5)
 
-time.sleep(1)  # Pause between movements
+# # Test servo positions
+# print("Moving legs out...")
+# set_servo_positions(legs_out)
 
-print("Moving legs in...")
-set_servo_positions(legs_in)
+# time.sleep(1)  # Pause between movements
 
-GPIO.cleanup()
+# print("Moving legs in...")
+# set_servo_positions(legs_in)
+
+# GPIO.cleanup()
